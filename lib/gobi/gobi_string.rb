8dc2f17b9626c4a26f7ce8904b5c709a4e01a1e3
NaotoCoding <naoto.zaniah@gmail.com>
2024-02-17 09:54:08 +0900

    deyansuメソッド、nariメソッドを作成

---
+++ lib/gobi/gobi_string.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class String
+  def deyansu
+    "#{self}でやんす"
+  end
+
+  def nari
+    "#{self}なり"
+  end
+end
--- spec/gobi_spec.rb
+++ spec/gobi_spec.rb
@@ -5,7 +5,15 @@ RSpec.describe Gobi do
     expect(Gobi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#deyansu" do
+    it "文字列の語尾に'でやんす'をつけたものを返す" do
+      expect("お団子".deyansu).to eq "お団子でやんす"
+    end
+  end
+
+  describe "#nari" do
+    it "文字列の語尾に'なり'をつけたものを返す" do
+      expect("お団子".nari).to eq "お団子なり"
+    end
   end
 end
