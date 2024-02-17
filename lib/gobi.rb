3230b23bbe500fc2d6b6d9d58b9154b60dd15cec
NaotoCoding <naoto.zaniah@gmail.com>
2024-02-17 09:53:08 +0900

    gobi_string.rbを作成して読み込み

--- lib/gobi.rb
+++ lib/gobi.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "gobi/version"
+require_relative "gobi/gobi_string"
 
-module Gobi
-  class Error < StandardError; end
-  # Your code goes here...
-end
+module Gobi; end
