05660af5b8d10e606216b05cb48fae5b6c38f548
NaotoCoding <naoto.zaniah@gmail.com>
2024-10-05 09:37:59 +0900

    gobiの設定ファイルをmain.rbに集約

---
+++ lib/gobi/main.rb
@@ -0,0 +1,41 @@
+class String
+  def ja
+    "#{self}じゃ"
+  end
+
+  def dabe
+    "#{self}だべ"
+  end
+
+  def zura
+    "#{self}ずら"
+  end
+
+  def yanen
+    "#{self}やねん"
+  end
+
+  def bai
+    "#{self}ばい"
+  end
+
+  def tai
+    "#{self}たい"
+  end
+
+  def deyansu
+    "#{self}でやんす"
+  end
+
+  def nari
+    "#{self}なり"
+  end
+
+  def degozaru
+    "#{self}でござる"
+  end
+
+  def desuwa
+    "#{self}ですわ"
+  end
+end
